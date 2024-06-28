17cb629fa5e856a2f5bc18246c60259076cb5fc1
SachaDee <sachadehe@gmail.com>
2024-06-28 19:42:09 -0300

    Update face_landmark_estimator.py
    np.float to float

--- ptgaze/head_pose_estimation/face_landmark_estimator.py
+++ ptgaze/head_pose_estimation/face_landmark_estimator.py
@@ -57,10 +57,10 @@ class LandmarkEstimator:
         for bbox in bboxes:
             predictions = self.predictor(image[:, :, ::-1], bbox)
             landmarks = np.array([(pt.x, pt.y) for pt in predictions.parts()],
-                                 dtype=np.float)
+                                 dtype=float)
             bbox = np.array([[bbox.left(), bbox.top()],
                              [bbox.right(), bbox.bottom()]],
-                            dtype=np.float)
+                            dtype=float)
             detected.append(Face(bbox, landmarks))
         return detected
 
@@ -77,7 +77,7 @@ class LandmarkEstimator:
             predictions = []
         detected = []
         for bbox, landmarks in zip(bboxes, predictions):
-            bbox = np.array(bbox, dtype=np.float).reshape(2, 2)
+            bbox = np.array(bbox, dtype=float).reshape(2, 2)
             detected.append(Face(bbox, landmarks))
         return detected
 
